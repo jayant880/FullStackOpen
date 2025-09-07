a6ee41ae3a524eee8d62d3b8508defad341bb243
Jayant <188503548+jayant880@users.noreply.github.com>
2025-09-07 20:45:21 +0000

    2.2: Course information step 7

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,15 @@
 import Content from "./Content";
 import Header from "./Header";
+import Total from "./Total";
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <strong>
+        <Total parts={course.parts} />
+      </strong>
     </div>
   );
 };
