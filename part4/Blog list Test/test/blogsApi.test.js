043df9a7f35172b3e66e1c0afc228d07faa32d39
Jayant <188503548+jayant880@users.noreply.github.com>
2025-09-26 12:07:39 +0000

    4.10: Blog List Tests, step 3

--- part4/Blog list Test/test/blogsApi.test.js
+++ part4/Blog list Test/test/blogsApi.test.js
@@ -1,9 +1,11 @@
 const { test, describe, beforeEach, after } = require('node:test');
 const assert = require('node:assert');
-const mongooes = require('mongoose');
+const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require("../app");
 const Blog = require('../model/blog');
+const { blogsInDb } = require('../utils/test_helper');
+const { info } = require('../utils/logger');
 
 const api = supertest(app);
 
@@ -19,8 +21,28 @@ test('all blogs are returned', async () => {
 test('unique identifier property of blog posts is named id', async () => {
     const response = await api.get('/api/blogs');
     const blog = response.body[0];
-    console.log(blog);
     assert.strictEqual(blog.id !== undefined, true);
     assert.strictEqual(blog._id, undefined);
 });
 
+describe('addition of new blog', () => {
+    test('succeeds with valid data', async () => {
+        const newBlog = {
+            title: 'Test Driven Development',
+            author: 'Tester',
+            url: 'http://test.helper.com',
+            likes: 100
+        };
+        await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/);
+
+        const blogAtEnd = await blogsInDb();
+        assert.strictEqual(blogAtEnd.length, 3);
+
+        const titles = blogAtEnd.map(b => b.title);
+        assert.strictEqual(titles.includes('Test Driven Development'), true);
+    })
+})
+
+after(async () => {
+    await mongoose.connection.close();
+})
