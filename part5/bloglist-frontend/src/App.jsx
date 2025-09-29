f7daf680a67fbf70667242bf60a8b0e96dbdbcef
Jayant <188503548+jayant880@users.noreply.github.com>
2025-09-29 22:22:45 +0000

    5.3: Blog List Frontend, step 3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
+import BlogForm from "./components/BlogForm";
 import blogService from "./services/blogs";
 import Login from "./components/Login";
 
@@ -13,6 +14,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON);
       setUser(user);
+      blogService.setToken(user.token);
     }
   }, []);
 
@@ -33,14 +35,38 @@ const App = () => {
     setUser(null);
   };
 
+  const addBlog = (blogObject) => {
+    blogService
+      .create(blogObject)
+      .then((returnedBlog) => {
+        setBlogs(blogs.concat(returnedBlog));
+        setErrorMessage(
+          `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      })
+      .catch((error) => {
+        setErrorMessage("Error creating blog");
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
+  };
+
   return (
     <div>
       <h2>blogs</h2>
+
+      {errorMessage && <div className="error">{errorMessage}</div>}
 
       <p>
         {user.name} logged in
         <button onClick={handleLogout}>Logout</button>
       </p>
+
+      <BlogForm createBlog={addBlog} />
 
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
