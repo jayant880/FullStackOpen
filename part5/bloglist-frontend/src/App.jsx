21239f9e2900527dfe9e14861d94da3d61bd12cd
Jayant <188503548+jayant880@users.noreply.github.com>
2025-09-29 22:03:05 +0000

    5.1: Blog List Frontend, step 1

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import Login from "./components/Login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  if (user === null) {
+    return (
+      <div>
+        <Login setUser={setUser} setErrorMessage={setErrorMessage} />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ part5/bloglist-frontend/src/components/Login.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import loginService from "../services/login";
+
+const Login = ({ setUser, setErrorMessage }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+  return (
+    <>
+      <h1>Log in to application</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
